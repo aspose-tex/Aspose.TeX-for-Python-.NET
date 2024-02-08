530aa66d7019d93a5260885771efb6a7d3841a8f
Aleksey Kolodyazhnyy <alexey.kolodyazhnyy@aspose.com>
2024-02-08 16:02:58 +0700

    TEXNET-312 Upload the Python examples project and gists

--- Aspose.TeX.Examples/LoadLicenseFromStream/load_license_from_stream.py
+++ Aspose.TeX.Examples/LoadLicenseFromStream/load_license_from_stream.py
@@ -9,7 +9,7 @@ class LoadLicenseFromStream:
         # Initialize license object.
         license = License()
         # Load license in FileStream.
-        with (open(Util.license_path, "rb")) as my_stream:
+        with open(Util.license_path, "rb") as my_stream:
             # Set license.
             license.set_license(my_stream)
         print('License set successfully.')
